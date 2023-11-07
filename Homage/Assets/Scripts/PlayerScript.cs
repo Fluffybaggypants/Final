53ec9ab4bb2d354ff5dc0bbb0e3b351fbc9e07dc
STUDENT\liu <]>
2023-11-07 17:46:29 -0500

    Movement

--- Homage/Assets/Scripts/PlayerScript.cs
+++ Homage/Assets/Scripts/PlayerScript.cs
@@ -5,58 +5,58 @@ using UnityEngine.SceneManagement;
 
 public class PlayerScript : MonoBehaviour
 {
-     public Transform target;
-    public Rigidbody2D RB;
-     public float Speed = 5f;
-     public float restartDelay = 1f;
-    public ParticleSystem PS;
-    
+      public Transform target;
+public Rigidbody2D RB;
+ public float Speed = 5f;
+ public float restartDelay = 1f;
+public ParticleSystem PS;
 
-    // Start is called before the first frame update
-    void Start()
+
+// Start is called before the first frame update
+void Start()
+{
+    RB = GetComponent<Rigidbody2D>();
+}
+
+// Update is called once per frame
+void Update()
+{
+    RB.velocity = (new Vector2(10,RB.velocity.y));
+
+    //transform.position += transform.right * Speed * Time.deltaTime;
+   /* Vector2 vel = RB.velocity;
+    RB.AddForce(vel.x * Time.deltaTime,0,0);*/
+   
+  //transform.position = new Vector3(target.position.x, 0, transform.position.z);
+    if(Input.GetKeyDown(KeyCode.Space))
     {
-        RB = GetComponent<Rigidbody2D>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        RB.velocity = (new Vector2(10,RB.velocity.y));
-
-        //transform.position += transform.right * Speed * Time.deltaTime;
-       /* Vector2 vel = RB.velocity;
-        RB.AddForce(vel.x * Time.deltaTime,0,0);*/
-       
-      //transform.position = new Vector3(target.position.x, 0, transform.position.z);
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Mathf.Sign(RB.gravityScale) == 1)
         {
-            if (Mathf.Sign(RB.gravityScale) == 1)
-            {
-                RB.gravityScale = - RB.gravityScale;
-            }
-            else 
-            {
-                RB.gravityScale = Mathf.Abs(RB.gravityScale);
-            }
+            RB.gravityScale = - RB.gravityScale;
         }
-
-    }
-    void Restart()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
-
-    void OnCollisionEnter2D(Collision2D other)
-    {
-        if (other.gameObject.name == "Spikes")
+        else 
         {
-             Invoke("Restart", restartDelay);
-            PS.Emit(10);
-            //Destroy(gameObject);
-        }
-        if (other.gameObject.name == "Finished")
-        {
-
+            RB.gravityScale = Mathf.Abs(RB.gravityScale);
         }
     }
+
+}
+void Restart()
+{
+    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+}
+
+void OnCollisionEnter2D(Collision2D other)
+{
+    if (other.gameObject.name == "Spikes")
+    {
+         Invoke("Restart", restartDelay);
+        PS.Emit(10);
+        //Destroy(gameObject);
+    }
+    if (other.gameObject.name == "Finished")
+    {
+
+    }
+}
 }
